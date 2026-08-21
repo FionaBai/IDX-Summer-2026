7305571fa0087403900d04eaa53f77a007e32761
FionaBai <fbai2@illinois.edu>
2026-08-21 15:22:50 -0500

    test(filters): update tests for controlled filter state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,12 @@ import ErrorBoundary from "./components/ErrorBoundary";
 function App() {
   return (
     <ErrorBoundary>
-      <BrowserRouter>
+      <BrowserRouter
+        future={{
+          v7_startTransition: true,
+          v7_relativeSplatPath: true,
+        }}
+      >
         <Routes>
           <Route
             path="/"
